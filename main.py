d84070dbb2be6fc92955ff02b42361aef1226dca
Dejan Dudukovic <dekidudukovic@gmail.com>
2021-11-07 16:14:09 +0100

    Added the Speech-To-Text script with the bug#001 (logged).

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import encode
+import speech_recognition
+import pyttsx3
 
 if __name__ == "__main__":
-    pass
+    
+    # Init
+    recognizer = speech_recognition.Recognizer()
+    while True:
 
+        # Listening to the user speaking
+        try:
+
+            with speech_recognition.Microphone() as mic:
+
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+                
+                text = recognizer.recognize_google(audio)
+                text = text.lower()
+                
+                print(f"Recognized {text}")
+
+        except speech_recognition.UnknownValueError():
+        
+            recognizer = speech_recognition.Recognizer()
+            continue
